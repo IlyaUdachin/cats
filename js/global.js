9840636f683c7d3f4cfc55953de7ece25b4419a6
IlyaUdachin <ilya.udachin@gmail.com>
2023-04-08 16:46:29 +0300

    commit_2

--- js/global.js
+++ js/global.js
@@ -1,3 +1,7 @@
 const block = document.querySelector(".wrapper");
+const addBtn = document.querySelector(".add");
+const mdBox = document.querySelector(".modal-block");
+const mdClose = mdBox.querySelector(".modal-close");
+
 let name = "IlyaUdachin";
 let path = `https://cats.petiteweb.dev/api/single/${name}`;
