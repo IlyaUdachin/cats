a038a771327c341bdd4c525ea2ce030dde4e19fc
IlyaUdachin <ilya.udachin@gmail.com>
2023-04-11 20:48:56 +0300

    commit_3

--- js/global.js
+++ js/global.js
@@ -2,6 +2,10 @@ const block = document.querySelector(".wrapper");
 const addBtn = document.querySelector(".add");
 const mdBox = document.querySelector(".modal-block");
 const mdClose = mdBox.querySelector(".modal-close");
+const addForm = document.forms.add;
+const prevTag = addForm.querySelector(".preview");
+// console.log(addForm)
 
 let name = "IlyaUdachin";
 let path = `https://cats.petiteweb.dev/api/single/${name}`;
+
--- script.js
+++ script.js
@@ -22,3 +22,55 @@ mdBox.addEventListener("click", e => {
     mdBox.classList.remove("active");
   }
 });
+
+addForm.elements.image.addEventListener("change", e => {
+  prevTag.style.backgroundImage = `url(${e.currentTarget.value})`;
+})
+
+addForm.addEventListener("submit", e => {
+  e.stopPropagation();
+  e.preventDefault();
+  // console.log(addForm)
+  // console.log(e.currentTarget)
+  // console.log(addForm.children)
+  // console.log(addForm.elements);
+  const body = {}
+  for (let i = 0; i < addForm.elements.length; i++) {
+    const el = addForm.elements[i];
+    // console.log(el.name, el.value);
+    if (el.name) {
+      if (el.name === "favorite") {
+          body[el.name] = el.checked;
+      } else {
+          body[el.name] = el.value;
+      }
+    }
+  }
+  // console.log(body);
+
+  fetch(path + "/ids")
+    .then(res => res.json())
+    .then(ids => {
+      console.log(ids);
+      body.id = ids[ids.length - 1] + 1;
+      console.log(body);
+      return fetch(path + "/add", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+      })
+    })
+    .then(res => {
+      if (res.status === 200) {
+        addForm.reset();
+        prevTag.style = null;
+        mdBox.classList.remove("active");
+        createCard(body, block);
+      }
+    })
+  //  .then(res => {
+  //    console.log(res.status);
+  //  })
+})
